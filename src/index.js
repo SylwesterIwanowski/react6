9669b9652ca37faf6769988809406d0d30b5507c
SylwesterIwanowski <50060453+SylwesterIwanowski@users.noreply.github.com>
2021-12-22 13:52:31 +0100

    add math random

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Sylwek";
+const num = 8;
+const num1 = 2;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}</h1>
+    <p>Your lucki number is: {Math.floor(Math.random() * 10)} </p>
+  </div>,
+  document.getElementById("root")
+);
